96f016e9b009f3b3fee35670616fe5755ac6b0e5
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-08-19 12:13:35 -0700

    volume control error

--- app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
@@ -2,19 +2,48 @@ package com.andrewflincoln.audiodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.turnin_on_the_screw);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.volumeSeekBar);
+
+        volumeControl.setMax(maxVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seekbar changed", Integer.toString(i));
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }
