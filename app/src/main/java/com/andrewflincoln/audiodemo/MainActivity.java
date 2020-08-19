7c076de64faafb60a7471d87457bd8b235d475c4
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-08-19 11:11:33 -0700

    plays audio with controls

--- app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.turnin_on_the_screw);
+        mediaPlayer = MediaPlayer.create(this, R.raw.turnin_on_the_screw);
+
+
+    }
+
+    public void play(View view) {
+
         mediaPlayer.start();
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
     }
 }
