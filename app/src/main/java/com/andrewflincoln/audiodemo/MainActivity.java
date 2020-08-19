307a4f21cf62c9e3c6b3103d65df270bf70acc33
Andrew Lincoln <andrewjlincoln@gmail.com>
2020-08-19 11:00:16 -0700

    audio playing

--- app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
+++ app/src/main/java/com/andrewflincoln/audiodemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.andrewflincoln.audiodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.turnin_on_the_screw);
+        mediaPlayer.start();
     }
 }
